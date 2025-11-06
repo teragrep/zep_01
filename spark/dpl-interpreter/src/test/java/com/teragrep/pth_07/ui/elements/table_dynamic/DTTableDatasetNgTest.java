fad82ce753c285ceb4d5c9b0e7853fe2cd7efed9
Laukkala <140611897+Laukkala@users.noreply.github.com>
2025-11-06 13:16:40 +0200

    Issue 268 draw not being incremented (#269)
    * Fixed an issue where draw value of PARAGRAPH_UPDATE_OUTPUT messages were not being incremented when a new batch of data was received.
    
    Now draw value is incremented on each new batch of data and reset to 1 when the schema of the data is changed.
    
    * Fixed missing assertions from test
    
    * Replaced makeRowsList() and makeRowslist2() with a TestDPLData helper class which can generate different types of Datasets for testing purposes without having to always create new methods.
    
    * Made some fields final
    
    * Made some fields final in DTHeaderTest, renamed variables and added comments for more readability
    
    * Renamed draw to drawCount
    Added primary constructor for DTTableDatasetNg
    Made some fields final
    
    * Moved TestDPLData into its own file from within a test class
    
    * Made parameters final and added javadoc returns comments

--- spark/dpl-interpreter/src/test/java/com/teragrep/pth_07/ui/elements/table_dynamic/DTTableDatasetNgTest.java
+++ spark/dpl-interpreter/src/test/java/com/teragrep/pth_07/ui/elements/table_dynamic/DTTableDatasetNgTest.java
@@ -45,23 +45,17 @@
  */
 package com.teragrep.pth_07.ui.elements.table_dynamic;
 
+import com.teragrep.pth_07.ui.elements.table_dynamic.testdata.TestDPLData;
 import com.teragrep.zep_01.display.AngularObjectRegistry;
-import com.teragrep.zep_01.interpreter.InterpreterContext;
-import com.teragrep.zep_01.interpreter.InterpreterOutput;
-import com.teragrep.zep_01.interpreter.InterpreterOutputListener;
-import com.teragrep.zep_01.interpreter.InterpreterResultMessageOutput;
+import com.teragrep.zep_01.interpreter.*;
 import jakarta.json.Json;
 import jakarta.json.JsonArray;
 import jakarta.json.JsonArrayBuilder;
 import jakarta.json.JsonObject;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
-import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
-import org.apache.spark.sql.types.DataTypes;
-import org.apache.spark.sql.types.MetadataBuilder;
-import org.apache.spark.sql.types.StructField;
-import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.types.*;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -97,21 +91,19 @@ public class DTTableDatasetNgTest {
                     new StructField("origin", DataTypes.StringType, false, new MetadataBuilder().build())
             }
     );
-    private final List<Row> rows = makeRowsList(
-            0L, 				// _time
-            0L, 					// id
-            "data data", 			// _raw
-            "index_A", 				// index
-            "stream", 				// sourcetype
-            "host", 				// host
-            "input", 				// source
-            String.valueOf(0), 	    // partition
-            0L, 				    // offset
-            "test data",            // origin
-            49                     // make n amount of rows
-    );
+    private final TestDPLData testDataset = new TestDPLData(sparkSession, testSchema);
+    private final Dataset<Row> testDs = testDataset.createDataset(49,Timestamp.from(Instant.ofEpochSecond(0)),0L,"data data","index_A","stream","host","input",String.valueOf(0),0L,"test data");
 
-    Dataset<Row> testDs = sparkSession.createDataFrame(rows, testSchema);
+
+    private final StructType smallTestSchema = new StructType(
+            new StructField[] {
+                    new StructField("_time", DataTypes.TimestampType, false, new MetadataBuilder().build()),
+                    new StructField("id", DataTypes.LongType, false, new MetadataBuilder().build()),
+                    new StructField("_raw", DataTypes.StringType, false, new MetadataBuilder().build()),
+            }
+    );
+    private final TestDPLData smallTestDataset = new TestDPLData(sparkSession, smallTestSchema);
+    private final Dataset<Row> smallTestDs = smallTestDataset.createDataset(49,Timestamp.from(Instant.ofEpochSecond(0)),0L,"data data");
 
     @Test
     public void testAJAXResponse() {
@@ -165,21 +157,6 @@ public class DTTableDatasetNgTest {
         assertEquals(expectedJson.toString()
                 , response.toString()
         );
-    }
-
-    private List<Row> makeRowsList(long _time, Long id, String _raw, String index, String sourcetype, String host, String source, String partition, Long offset, String origin, long amount) {
-        ArrayList<Row> rowArrayList = new ArrayList<>();
-
-        while (amount > 0) {
-            // creates rows in inverse order
-            Timestamp timestamp = Timestamp.from(Instant.ofEpochSecond(_time+amount));
-            Row row = RowFactory.create(timestamp, id, _raw, index, sourcetype, host, source, partition, offset, origin);
-            rowArrayList.add(row);
-            amount--;
-        }
-
-
-        return rowArrayList;
     }
 
     @Test
@@ -237,6 +214,44 @@ public class DTTableDatasetNgTest {
         Assertions.assertEquals(2,listener.numberOfUpdateCalls());
         Assertions.assertEquals(0,listener.numberOfResetCalls());
 
+    }
+
+    /**
+     * An incremented 'draw' value should be sent to the UI each time a new batch of data is received.
+     * The 'draw' value should be reset to 1 every time the schema of the data changes.
+     */
+    @Test
+    public void testIncrementDraw(){
+        TestInterpreterOutputListener listener = new TestInterpreterOutputListener();
+        InterpreterOutput testOutput =  new InterpreterOutput(listener);
+
+        AngularObjectRegistry testRegistry = new AngularObjectRegistry("test", null);
+        InterpreterContext context = InterpreterContext.builder().setInterpreterOut(testOutput).setAngularObjectRegistry(testRegistry).build();
+        DTTableDatasetNg dtTableDatasetNg = new DTTableDatasetNg(context);
+
+        // Simulate DPL receiving new data.
+        Assertions.assertDoesNotThrow(()->{
+            dtTableDatasetNg.setParagraphDataset(testDs);
+        });
+        List<InterpreterResultMessage> messages = Assertions.assertDoesNotThrow(()->testOutput.toInterpreterResultMessage());
+        // First message should have draw value of 1
+        Assertions.assertTrue(messages.get(0).getData().contains("\"draw\":1"));
+
+        // Simulate DPL receiving another batch of new data without changing schema.
+        Assertions.assertDoesNotThrow(()->{
+            dtTableDatasetNg.setParagraphDataset(testDs);
+        });
+        List<InterpreterResultMessage> messages2 = Assertions.assertDoesNotThrow(()->testOutput.toInterpreterResultMessage());
+        // Second message should have draw value of 2
+        Assertions.assertTrue(messages2.get(0).getData().contains("\"draw\":2"));
+
+        // Simulate DPL receiving yet another batch of new data but with a changed schema.
+        Assertions.assertDoesNotThrow(()->{
+            dtTableDatasetNg.setParagraphDataset(smallTestDs);
+        });
+        List<InterpreterResultMessage> messages3 = Assertions.assertDoesNotThrow(()->testOutput.toInterpreterResultMessage());
+        // Third message's draw value should be reset to 1
+        Assertions.assertTrue(messages3.get(0).getData().contains("\"draw\":1"));
     }
 
     private class TestInterpreterOutputListener implements InterpreterOutputListener{
---
+++ spark/dpl-interpreter/src/test/java/com/teragrep/pth_07/ui/elements/table_dynamic/testdata/TestDPLData.java
@@ -0,0 +1,99 @@
+/*
+ * Teragrep DPL Spark Integration PTH-07
+ * Copyright (C) 2022  Suomen Kanuuna Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://github.com/teragrep/teragrep/blob/main/LICENSE>.
+ *
+ *
+ * Additional permission under GNU Affero General Public License version 3
+ * section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with other code, such other code is not for that reason alone subject to any
+ * of the requirements of the GNU Affero GPL version 3 as long as this Program
+ * is the same Program as licensed from Suomen Kanuuna Oy without any additional
+ * modifications.
+ *
+ * Supplemented terms under GNU Affero General Public License version 3
+ * section 7
+ *
+ * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
+ * versions must be marked as "Modified version of" The Program.
+ *
+ * Names of the licensors and authors may not be used for publicity purposes.
+ *
+ * No rights are granted for use of trade names, trademarks, or service marks
+ * which are in The Program if any.
+ *
+ * Licensee must indemnify licensors and authors for any liability that these
+ * contractual assumptions impose on licensors and authors.
+ *
+ * To the extent this program is licensed as part of the Commercial versions of
+ * Teragrep, the applicable Commercial License may apply to this file if you as
+ * a licensee so wish it.
+ */
+package com.teragrep.pth_07.ui.elements.table_dynamic.testdata;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.RowFactory;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.StructType;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public final class TestDPLData {
+        private final SparkSession sparkSession;
+        private final StructType schema;
+
+        public TestDPLData(final SparkSession sparkSession, final StructType schema){
+            this.sparkSession = sparkSession;
+            this.schema = schema;
+        }
+
+        /**
+         * Tries to generate a dataset of size 'amount' with default values corresponding to varargs 'values'
+         * @param amount - desired number of rows in the dataset
+         * @param values - varargs specifying the default values to fill into the dataset.
+         * @return A Dataset containing 'amount' number of Rows, each pre-populated with default values
+         */
+
+        public Dataset<Row> createDataset(final int amount, final Object ... values){
+            final List<Row> rows = rowList(amount,values);
+            return sparkSession.createDataFrame(rows, schema);
+        }
+
+        /**
+         * Generates a List of Rows based on given values.
+         * @param amount - Number of rows to generate
+         * @param values - Default values to add for each Row. If first value given is a Timestamp, it will be incremented by one for each Row.
+         * @return A list of Rows with an incrementing Timestamp and pre-populated with default values.
+         */
+        private List<Row> rowList(final int amount, final Object ... values){
+            final List<Object> valueList = Arrays.asList(values);
+            final ArrayList<Row> rowArrayList = new ArrayList<>();
+            for (int i = amount; i > 0; i--) {
+                if(valueList.get(0) instanceof Timestamp){
+                    valueList.set(0, Timestamp.from(Instant.ofEpochSecond(i)));
+                }
+                final Row row = RowFactory.create(valueList.toArray());
+                rowArrayList.add(row);
+            }
+            return rowArrayList;
+        }
+    }
