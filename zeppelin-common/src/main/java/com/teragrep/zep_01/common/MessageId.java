ad563784b726acac0a21978ff6fa4baf62616f0e
Laukkala <140611897+Laukkala@users.noreply.github.com>
2026-04-02 12:55:07 +0300

    Replaced own Stubable interface with one from stb_01

--- zeppelin-common/src/main/java/com/teragrep/zep_01/common/MessageId.java
+++ zeppelin-common/src/main/java/com/teragrep/zep_01/common/MessageId.java
@@ -1,5 +1,6 @@
 package com.teragrep.zep_01.common;
 
+import com.teragrep.stb_01.Stubable;
 import jakarta.json.JsonValue;
 
 public interface MessageId extends Stubable {
--- zeppelin-common/src/main/java/com/teragrep/zep_01/common/Stubable.java
+++
@@ -1,5 +0,0 @@
-package com.teragrep.zep_01.common;
-
-public interface Stubable {
-    public abstract boolean isStub();
-}
